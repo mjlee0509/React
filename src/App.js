0caaa0746f520451a620f213023b4fb613242b6f
mjlee0509 <mjlee0509@gmail.com>
2023-07-15 16:46:47 +0900

    05. 화면전환 & 라우팅 - react-route-dom 설치 - Link, Routes, Route 태그를 활용한 링크 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Info from './pages/Info';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/info" element={<Info />} />
+      </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,11 @@
+// 먼저 터미널에서 npm install react-router-dom 실행
+import { Link } from "react-router-dom" // HTML의 a태그 역할
+
 const Header = () => {
     return(
         <div className="header">
-            
+            <Link to="/">Home</Link>
+            <Link to="/info">Info</Link>
         </div>
     );
 }
